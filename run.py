818876beeea4489824ba992489b7d3bd31cc3ff9
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-06-08 19:54:56 +0000

    Refarctoring code

--- run.py
+++ run.py
@@ -83,6 +83,15 @@ def update_surplus_worksheet(data):
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
 
+def update_worksheet(data, worksheet):
+    """
+    Calculate the surplus
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet is updated")
+
 
 def main():
     """
@@ -90,11 +99,11 @@ def main():
     """
     data = get_sales_data()   
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data,"sales")
     calculate_surplus_data(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
 
 print("Welecome to Love Sandwiches Data Automation")
 main()   
