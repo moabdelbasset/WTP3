2bf0363d05a2de8fe584e9dd7db6c1ad6ef80d00
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-06-08 19:34:33 +0000

    get stock data

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials 
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -58,7 +59,25 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Calculate the surplus
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    #stock_row = stock[len(stock)-1]
+    stock_row = stock[-1]
+    print(stock_row)
 
-data = get_sales_data()   
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()   
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print("Welecome to Love Sandwiches Data Automation")
+main()   
