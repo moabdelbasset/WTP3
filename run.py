8ab7814d493e90823cfb0cd2d8a75a23193791da
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-06-09 04:36:14 +0000

    Updating stock sheet

--- run.py
+++ run.py
@@ -103,6 +103,19 @@ def get_last_5_entries_sales():
         columns.append(column[-5:])
     return columns   
 
+def calculate_stock_data(data):
+    """
+    Calculate average stock for each item
+    """
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data 
 
 def main():
     """
@@ -115,7 +128,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_Data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_Data, "stock")
 
 print("Welecome to Love Sandwiches Data Automation")
-#main()   
-sales_columns = get_last_5_entries_sales()
+main()   
